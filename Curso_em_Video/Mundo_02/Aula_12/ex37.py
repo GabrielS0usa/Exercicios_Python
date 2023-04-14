f95bd6fed9dbaedfd3806631b747f7e817474bc2
Gabriel_Sousa <103265192+GabrielS0usa@users.noreply.github.com>
2023-04-14 12:02:59 -0300

    Update ex37.py

--- Curso_em_Video/Mundo_02/Aula_12/ex37.py
+++ Curso_em_Video/Mundo_02/Aula_12/ex37.py
@@ -1,3 +1,7 @@
+# Escreva um programa em Python que leia um número inteiro qualquer e peça para o
+# usuário escolher qual será a base de conversão:
+# 1 para binário, 2 para octal e 3 para hexadecimal.
+
 x = int(input('Entre com um valor: '))
 c = int(input('''
 - 1 para binário
