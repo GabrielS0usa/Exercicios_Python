270696f1f37c8ecd00cbd948c52a4be8138a5478
Gabriel_Sousa <103265192+GabrielS0usa@users.noreply.github.com>
2023-04-14 12:04:05 -0300

    Update ex44.py

--- Curso_em_Video/Mundo_02/Aula_12/ex44.py
+++ Curso_em_Video/Mundo_02/Aula_12/ex44.py
@@ -1,3 +1,11 @@
+# Elabore um programa que calcule o valor a ser pago por um produto,
+# considerando o seu preço normal e condição de pagamento:
+# – à vista dinheiro/cheque: 10% de desconto
+# – à vista no cartão: 5% de desconto
+# – em até 2x no cartão: preço formal
+# – 3x ou mais no cartão: 20% de juros
+
+print(f'{" LOJAS GABRIEL ":=^40}')
 produto = float(input('Digite o valor do produto: R$'))
 print('-=- Selecione a forma de pagamento -=-')
 print('''
@@ -6,7 +14,7 @@ print('''
 3- 2x no cartão
 4- 3x ou mais no cartão''')
 print('-=-' * 20)
-forma = float(input('Selecione uma opção de 1 a 4: '))
+forma = int(input('Selecione uma opção de 1 a 4: '))
 if forma == 1:
     desconto = produto * 10 / 100
     print(f'Com pagamento à vista fica R${produto - desconto:.2f} .')
@@ -14,11 +22,11 @@ elif forma == 2:
     desconto = produto * 5 / 100
     print(f'Com pagamento à vista no cartão fica R${produto - desconto:.2f} .')
 elif forma == 3:
-    print(f'Com o pagamento em 2x sem juros fica duas parcelas de R${produto / 3 :.2f} .')
+    print(f'Com o pagamento em 2x sem juros fica duas parcelas de R${produto / 2 :.2f} .')
 elif forma == 4:
     vezes = int(input('Quantas vezes deseja dividir: '))
     juros = produto * 20 / 100
-    produto = produto + juros
-    print(f'Com o pagamento em {vezes}x ou mais vezes fica R${produto + juros:.2f} em {vezes}x de R${(produto + juros) / vezes:.2f} ')
+    prec = produto + juros
+    print(f'Com o pagamento em {vezes}x ou mais vezes fica R${prec:.2f} em {vezes}x de R${prec / vezes:.2f} ')
 else:
     print('\033[31mOpção invalida.\033[m')
