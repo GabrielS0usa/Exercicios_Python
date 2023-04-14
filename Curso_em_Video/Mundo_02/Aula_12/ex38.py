1bfadfc6b62486abef2e1ea3aa1a843c1f7185cf
Gabriel_Sousa <103265192+GabrielS0usa@users.noreply.github.com>
2023-04-14 12:03:29 -0300

    Update ex38.py

--- Curso_em_Video/Mundo_02/Aula_12/ex38.py
+++ Curso_em_Video/Mundo_02/Aula_12/ex38.py
@@ -1,3 +1,8 @@
+# Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+# – O primeiro valor é maior
+# – O segundo valor é maior
+# – Não existe valor maior, os dois são iguais
+
 print('Digite dois números e eu vou te falar o maior.')
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
@@ -7,3 +12,4 @@ elif n2 > n1:
     print(f'O segundo número é o maior.')
 elif n1 == n2:
     print('Não tem um número maior, os dois são iguais.')
+
